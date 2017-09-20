5961b090f116ea551f5c70c5bad4577e50eca13b
Joe Bosman <joseph.a.bosman@gmail.com>
2017-09-20 16:45:51 -0400

    GPIO trial.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello Pi!');
+const { GPIO } = require('onoff');
+const LED = new GPIO(4,'out');
+let interval;
+
+interval = setInterval(function(){
+	let value = (LED.readSync() + 1) % 2;
+
+	LED.write(value, function(){
+		console.log("Changed LED state to: " + value);
+	});
+}, 2000);
+
+process.on('SIGINT', function(){
+	clearInterval(interval);
+	LED.writeSync(0);
+	LED.unexport();
+	console.log('Bye, bye!');
+	process.exit();
+});
+
+
